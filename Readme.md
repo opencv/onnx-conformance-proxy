e1c8612c93ab178f8118624c6f47ad7153e2ff24
Alexander Smorkalov <alexander.smorkalov@opencv.ai>
2026-04-07 16:20:34 +0300

    Initial OpenCV ONNX scoreboard adapter version.

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Intro
+
+The reprositry contains adapter on top of OpenCV to run accuracy tests in ONNX backend-scoreboard
+infrastructure. The code is organized as Python package and may be used with OpenCV 4.x and 5.x
+branches.
+
+# References
+
+- ONNX backend scoreboard repo: https://github.com/onnx/backend-scoreboard
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='opencv-onnx-conformance-proxy',
+      version='0.1',
+      description='OpenCV DNN wrapper for ONNX conformace testing',
+      url='http://github.com/asmorkalov/backend-scoreboard',
+      author='Egor Smironov, Alexnader Smorkalov',
+      author_email='alexander.smorkalov@opencv.ai',
+      license='Apache 2.0',
+      packages=['opencv_onnx_proxy'],
+      zip_safe=False)
